0214341cdb537f45920d9f3b008d894e06dacf59
hanfly0702 <hanfly0702@gmail.com>
2024-02-27 10:26:44 +0800

    first commit

---
+++ mybatis/src/main/java/org/hyf/mybatis/SpringbootApplication.java
@@ -0,0 +1,20 @@
+package org.hyf.mybatis;
+
+import ch.qos.logback.core.pattern.FormatInfo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+
+
+
+
+
+	}
+}
